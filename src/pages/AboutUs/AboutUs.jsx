1937e3d66fcf804f14ef1d7659240d4d706590d7
SharikaJahanAunto <Sharikajahannn@gmail.com>
2023-05-04 09:17:36 +0600

    completed about section

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import MainContent from '../Shared/MainContent/MainContent';
+import './AboutUs.css'
+
 
 const AboutUs = () => {
     return (
-        <div>
-             
+        <div className="about-us">
+            <h1>About Us</h1>
+            <p>Welcome to BD FOOD, a platform dedicated to showcasing the incredible talent of Bangladeshi chefs. Our mission is to promote the rich culinary heritage of Bangladesh and connect food lovers with talented chefs who can bring these delicious flavors to life.</p>
+            <h2>Our Story</h2>
+            <p>BD FOOD was founded in 2021 by a group of food enthusiasts who wanted to create a space where people could discover and learn about Bangladeshi cuisine. We believe that food is not just about sustenance, but also about culture, community, and creativity. We wanted to create a platform that celebrates the artistry and passion of Bangladeshi chefs, and helps them to connect with audiences around the world.</p>
+            <h2>Our Team</h2>
+            <p>Our team is made up of food lovers, technologists, and entrepreneurs who are passionate about bringing people together through food. We work closely with our partner chefs to curate their menus and showcase their unique talents. We also collaborate with local farmers, suppliers, and other food artisans to promote sustainable and ethical food practices.</p>
+            <h2>Contact Us</h2>
+            <p>If you have any questions or feedback, we would love to hear from you! Please email us at <a href="mailto:info@bdfood.com">BDFood456@gmail.com</a> or follow us on social media:</p>
+          
         </div>
     );
 };
